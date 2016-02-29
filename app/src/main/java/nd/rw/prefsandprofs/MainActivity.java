97c1d24c7fedf78879728a25e27473ebf5ba1d42
mr-ndrw <andrew.torski@gmail.com>
2016-02-29 18:22:29 +0100

    Baaah

--- app/src/main/java/nd/rw/prefsandprofs/MainActivity.java
+++ app/src/main/java/nd/rw/prefsandprofs/MainActivity.java
@@ -1,5 +1,7 @@
 package nd.rw.prefsandprofs;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public EditText et_value;
+    public SharedPreferences prefs;
+    public String storedValue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +25,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        et_value = (EditText) findViewById(R.id.edit_text);
+        prefs = getPreferences(Context.MODE_PRIVATE);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        storedValue = prefs.getString(getString(R.string.stored_value_name), "No value present");
+        et_value.setText(storedValue);
+
     }
 
     @Override
@@ -48,5 +53,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void saveValue(View view) {
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(getString(R.string.stored_value_name), et_value.getText().toString());
+        editor.commit();
     }
 }
