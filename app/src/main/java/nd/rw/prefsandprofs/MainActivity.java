47c1cacc8fbfc7a854bbd5eb93a81f4745f4e3fa
mr-ndrw <andrew.torski@gmail.com>
2016-04-05 20:29:48 +0200

    Figured out Shared Preferences

--- app/src/main/java/nd/rw/prefsandprofs/MainActivity.java
+++ app/src/main/java/nd/rw/prefsandprofs/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +15,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     public EditText et_value;
     public SharedPreferences prefs;
     public String storedValue;
@@ -25,10 +27,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         et_value = (EditText) findViewById(R.id.edit_text);
+
         prefs = getPreferences(Context.MODE_PRIVATE);
 
         storedValue = prefs.getString(getString(R.string.stored_value_name), "No value present");
+        Log.d(TAG, "onCreate: retained value: " + storedValue);
         et_value.setText(storedValue);
 
     }
@@ -58,6 +63,34 @@ public class MainActivity extends AppCompatActivity {
     public void saveValue(View view) {
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString(getString(R.string.stored_value_name), et_value.getText().toString());
-        editor.commit();
+        editor.apply();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called with");
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(getString(R.string.stored_value_name), et_value.getText().toString());
+        Log.d(TAG, "onStop(): value put: " + et_value.getText().toString());
+        editor.apply();
+    }
+
+    /**
+     * Dispatch onPause() to fragments.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: onDestroy() called");
+//        SharedPreferences.Editor editor = prefs.edit();
+//        editor.putString(getString(R.string.stored_value_name), et_value.getText().toString());
+//        Log.d(TAG, "onDestroy: value put: " + et_value.getText().toString());
+//        editor.apply();
+        super.onDestroy();
     }
 }
